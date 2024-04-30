d3ee8129dfb67795cea683f01481d512faa4f309
langthan123 <95601383+langthan123@users.noreply.github.com>
2024-04-30 21:32:58 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Bé Nhi Ngốc Ngếch",
+    introTitle: 'Chin Chao Bé iu',
+    introDesc: `Hôm nay là một ngày rất rất rất đặc biệt nun ó.
+    Nên Hai làm cái này gửi tặng Bé và thay lời Hai muốn nói nà :3`,
+    btnIntro: 'Bắt đầu thoaiii <3',
+    title: 'Chúc mừng sinh nhật Bé, ngày một cô công chúa đã ra đời 🥰',
+    desc: 'Bé đã tròn 18 tuổi rồi đó, chúc Bé luôn nở nụ cười trên môi nhé, chúc Bé tất cả <3, thế là phải tập làm người lớn roài hông còn bị gọi là con nít nữa. Ngoại trừ Hai đúng hăm nà :3',
+    btnYes: 'Dạ đúng roài <33',
+    btnNo: 'Hông nha -.-',
+    question: 'Hai bít moà. Vậy Bé nghĩ sao nếu như Bé nhận được một lời tỏ tình nhân ngày sinh nhật ngay trên này nà 🤭',
+    btnReply: 'Gửi cho Hai na <3',
+    reply: 'Tất nhiên là phải đồng ý ròi',
+    mess: 'Hai biết là Bé sẽ hông từ chối moà 🥰',
+    messDesc: 'Từ chỗ này mình sẽ đổi cách xưng hô chút nah. Anh muốn nói với Bé điều này từ lâu lắm rồi. "Bé hãy làm người yêu anh nhé", nếu đồng ý hãy trả lời cho anh biết nhé, hoặc Bé có thể suy nghĩ thêm cũng được nà 😊',
+    btnAccept: 'Okiiiii nun <3',
+    messLink: 'https://www.facebook.com/messages/t/100015056085493/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
