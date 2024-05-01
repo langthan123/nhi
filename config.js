8336c68aaadfa1d78c722f85ff1780c85ab561d7
langthan123 <95601383+langthan123@users.noreply.github.com>
2024-05-01 14:42:01 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    titleWeb: "Bé Nhi Ngốc Ngếch",
+    introTitle: 'Chin Chao Bé iu',
+    introDesc: `Hôm nay là một ngày rất rất rất đặc biệt nun ó.
+    Nên Hai làm cái này gửi tặng Bé và thay lời Hai muốn nói nà :3`,
+    btnIntro: 'Bắt đầu thoaiii <3',
+    title: 'Chúc mừng sinh nhật Bé, ngày một cô công chúa đã ra đời 🥰',
+    desc: 'Thế là phải tập làm người lớn roài, hông còn bị gọi là con nít nữa. Ngoại trừ Hai đúng hăm nà :3',
+    btnYes: 'Dạ đúng roài <33',
+    btnNo: 'Hông nha -.-',
+    question: 'Hai bít moà. Vậy Bé muốn nhận món quà gì nhân ngày sinh nhật đặc biệt nà 🤭',
+    btnReply: 'Gửi cho Hai na <3',
+    reply: 'Tất nhiên là Hai ròi :3',
+    mess: 'Hai biết là Bé sẽ hông từ chối moà 🥰',
+    messDesc: 'Chúc Bé sinh nhật tuổi 18 thật nhiều niềm vui, luôn nở nụ cười trên môi, sớm tìm được ước mơ của mình là gì. Hành trình mới của Bé sẽ bắt đầu, mong Bé luôn vững bước trên con đường của mình nhé 🥰🥰',
+    btnAccept: 'Okiiiii nun <3',
+    messLink: 'https://langthan123.github.io/bla/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
